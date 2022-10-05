66dc8c112635d6f275ee34f57e9ded66ce356cf6
Admin <nguyenvantruongqt@gmail.com>
2022-10-05 23:37:47 +0700

    Build Login Screen UI

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,9 @@
 export const images = {
     auth_bg: require('./images/auth_bg.png'),
     auth_logo: require('./images/auth_logo.png'),
+    user_email: require('./images/user_email.png'),
+    user_password: require('./images/user_password.png'),
+    show_password: require('./images/show_password.png'),
 }
 
 export const fonts = {
@@ -12,6 +15,12 @@ export const fonts = {
 
 export const colors = {
     white: 'white',
+    cD6DFFF: '#D6DFFF',
+    cD6E1FF: '#D6E1FF',
+    textInputBg: 'rgba(255, 255, 255, 0.1)',
+    textInputBorder: 'rgba(255, 255, 255, 0.2)',
+    
+
     white_50_percent: 'rgba(255, 255, 255, 0.5)',
     c647FFF: '#647FFF',
     authen_gradient: {
