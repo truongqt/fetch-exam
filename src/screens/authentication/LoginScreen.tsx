c8055d68269e3bb1e5532e881e14a71b317099e1
Truong <nguyenvantruongqt@gmail.com>
2022-10-10 21:23:04 +0700

    Build Login Screen UI (#1)

--- src/screens/authentication/LoginScreen.tsx
+++ src/screens/authentication/LoginScreen.tsx
@@ -1,24 +1,68 @@
-import {Button, Image, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import {useDispatch} from 'react-redux';
-import allActions from 'redux-manager/allActions';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
+import {colors, fonts, images} from 'assets';
 import {
   MarketSummariesScreenName,
   StackParamList,
 } from 'navigation/ScreenProps';
+import React, {useState} from 'react';
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {useDispatch} from 'react-redux';
+import allActions from 'redux-manager/allActions';
 import {scale} from 'utils/helpers/device';
-import {colors, fonts, images} from 'assets';
 
 const LoginScreen = () => {
   const distpatch = useDispatch();
   const navigation = useNavigation<StackNavigationProp<StackParamList>>();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const renderEmailInput = () => (
+    <View style={[styles.textInputContainer, {marginTop: scale(60)}]}>
+      <TextInput
+        keyboardType="email-address"
+        placeholder="Email"
+        placeholderTextColor={colors.cD6E1FF}
+        style={styles.textInput}
+      />
+      <Image source={images.user_email} style={styles.textInputIcon} />
+    </View>
+  );
+
+  const renderPasswordInput = () => (
+    <View style={[styles.textInputContainer, {marginTop: scale(10)}]}>
+      <TextInput
+        secureTextEntry={!showPassword}
+        placeholder="Password"
+        placeholderTextColor={colors.cD6E1FF}
+        style={[styles.textInput, {paddingRight: scale(40)}]}
+      />
+      <Image source={images.user_password} style={styles.textInputIcon} />
+      <TouchableOpacity
+        style={styles.showPasswordContainer}
+        onPress={() => {
+          setShowPassword(!showPassword);
+        }}>
+        <Image source={images.show_password} style={styles.showPasswordIcon} />
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
       <Image source={images.auth_bg} style={styles.authBg} />
       <Image source={images.auth_logo} style={styles.authLogo} />
       <Text style={styles.title}>Sign in</Text>
+      <Text style={styles.subTitle}>Please sign in to continue</Text>
+      {renderEmailInput()}
+      {renderPasswordInput()}
       <Button
         title="Test"
         onPress={() => {
@@ -42,8 +86,6 @@ export default LoginScreen;
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    // justifyContent: 'center',
-    // alignItems: 'center'
   },
   authBg: {
     width: '100%',
@@ -59,10 +101,62 @@ const styles = StyleSheet.create({
     marginTop: scale(75),
   },
   title: {
-    // fontFamily: fonts.Roboto.bold,
-    fontWeight: '400',
-    fontSize: scale(22),
-    lineHeight: scale(26.4),
+    fontFamily: fonts.Roboto.bold,
+    fontWeight: '900',
+    fontSize: scale(23),
+    lineHeight: scale(30),
     color: colors.white,
+    textAlign: 'center',
+    marginTop: scale(24),
+    letterSpacing: scale(0.5),
+  },
+  subTitle: {
+    fontFamily: fonts.Roboto.regular,
+    fontWeight: '500',
+    fontSize: scale(16),
+    lineHeight: scale(24),
+    color: colors.cD6DFFF,
+    textAlign: 'center',
+    marginTop: scale(9),
+    letterSpacing: scale(0.3),
+  },
+  textInputContainer: {
+    width: scale(355),
+    height: scale(47),
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  textInput: {
+    fontFamily: fonts.Roboto.regular,
+    fontWeight: '500',
+    fontSize: scale(15),
+    lineHeight: scale(18),
+    letterSpacing: scale(0.2),
+    color: colors.cD6E1FF,
+    borderColor: colors.textInputBorder,
+    backgroundColor: colors.textInputBg,
+    borderWidth: scale(1.5),
+    borderRadius: scale(6),
+    width: scale(355),
+    height: scale(47),
+    paddingLeft: scale(40),
+    paddingRight: scale(13),
+  },
+  textInputIcon: {
+    width: scale(17),
+    height: scale(19),
+    resizeMode: 'contain',
+    position: 'absolute',
+    left: scale(14),
+  },
+  showPasswordContainer: {
+    position: 'absolute',
+    right: scale(17),
+  },
+  showPasswordIcon: {
+    width: scale(19),
+    height: scale(13),
+    resizeMode: 'contain',
   },
 });
