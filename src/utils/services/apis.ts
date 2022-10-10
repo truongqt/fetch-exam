7560403064609fa62e61c4e3d4b67df0465cc796
Admin <nguyenvantruongqt@gmail.com>
2022-10-10 22:37:40 +0700

    Saga login

--- src/utils/services/apis.ts
+++ src/utils/services/apis.ts
@@ -4,33 +4,58 @@ import { BASE_URL, ENDPOINTS } from 'utils/helpers/constants';
 
 export const apisauces = create({
   baseURL: BASE_URL,
-  headers: DEFAULT_HEADERS,
+  headers: {
+    ...DEFAULT_HEADERS,
+  },
   timeout: 3000,
 });
 
 type NetworkPromiseResponse<T> = Promise<T>;
 
-function login<T>(data: LoginParams): NetworkPromiseResponse<T>{
-    return new Promise((resolve, reject) => {
-        apisauces.post(ENDPOINTS.LOG_IN, data)
-        .then((res: any)=>{
-            console.log('api res: ', JSON.stringify(res))
-            if (!res || (res && res.data?.status === 'error')) {
-                return reject(res);
-              }
-              if (res && res.accessToken) {
-                // network.setToken(res.accessToken);
-                apisauces.setHeader("Authorization", `Bearer ${res.accessToken}`)
-              }
-            resolve(res)
-        })
-        .catch(error=>{
-            console.log('api error: ', JSON.stringify(error))
-            reject(error)
-        })
-    })
+function login<T>(data: LoginParams): NetworkPromiseResponse<T> {
+  return new Promise((resolve, reject) => {
+    apisauces.post(ENDPOINTS.LOG_IN, data)
+      .then((res) => {
+        // console.log('a: ', JSON.stringify(res.config))
+        // console.log('b: ', JSON.stringify(res.data))
+        // console.log('c: ', JSON.stringify(res.duration))
+        // console.log('c: ', JSON.stringify(res.headers))
+        // console.log('d: ', JSON.stringify(res.ok))
+        // console.log('e: ', JSON.stringify(res.originalError))
+        // console.log('f: ', JSON.stringify(res.problem))
+        // console.log('g: ', JSON.stringify(res.status))
+        if (res.ok) {
+          return resolve(res.data as any)
+        }
+        console.log('login api not OK')
+        return reject(res.data)
+      })
+      .catch(error => {
+        console.log('login api error: ', JSON.stringify(error))
+        return reject(error)
+      })
+  })
+}
+
+function getMarketSummaries<T>(): NetworkPromiseResponse<T> {
+  return new Promise((resolve, reject) => {
+    apisauces.get(ENDPOINTS.GET_MARKET_SUMMARIES)
+      .then((res) => {
+        console.log('getMarketSummaries api res: ', JSON.stringify(res))
+        if (res.ok) {
+          return resolve(res.data as any)
+        }
+        console.log('getMarketSummaries api not OK')
+        reject(res.data);
+      })
+      .catch(error => {
+        console.log('getMarketSummaries api error: ', JSON.stringify(error))
+        reject(error)
+      })
+  })
 }
 
 export const apis = {
-    login
+  login,
+  getMarketSummaries
 }
